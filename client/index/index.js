1a6d46f760db6388fb66cf47509ee47dbc88499b
Matt Walther <matt@mashio.net>
2016-02-15 14:11:27 -0800

    Tutorial showing show events work.

--- client/index/index.js
+++ client/index/index.js
@@ -3,3 +3,11 @@ Template.index.helpers({
         return userLoggedIn();
     }
 })
+
+Template.index.events({
+    'click h3': function(event){
+        event.preventDefault();
+        alert("You clicked me");
+        return;
+    }
+})
