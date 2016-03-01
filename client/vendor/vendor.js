2af78a329bfdba9c40415f8a5abd54d62aefe5d3
Matt Walther <matt@mashio.net>
2016-02-29 23:57:23 -0800

    vendor table milestone

--- client/vendor/vendor.js
+++ client/vendor/vendor.js
@@ -5,3 +5,12 @@ Template.vendor.helpers({
 Template.vendor.events({
     
 })
+
+
+Template.vendorTable.events({
+    'click tbody tr': function(event){
+        event.preventDefault();
+        console.log(event);
+        //var vendorId = $()
+    }
+})
