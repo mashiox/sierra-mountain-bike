76844ec91687b886c1af111fc3abce96825da2a2
Matt Walther <matt@mashio.net>
2016-03-03 23:26:39 -0800

    Implements some CSS normalization, and alters @bflo70 's html to be more meteor structured

--- client/ticket/ticket.js
+++ client/ticket/ticket.js
@@ -3,5 +3,21 @@ Template.ticket.helpers({
 })
 
 Template.ticket.events({
+    'click li': function(event){
+        event.preventDefault();
+        $("div#displayTable").hide();
+        $("div.table"+event.currentTarget.id).show();
+    }
+})
+
+Template.problems.events({
+    'click button#stats': function(event){
+        event.preventDefault();
+        alert("Stats Summary Clicked");
+    },
     
+    'click button#addProblem': function(event){
+        event.preventDefault();
+        alert('Add Typical Problem Clicked');
+    }
 })
