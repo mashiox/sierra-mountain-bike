279043d52f1426dc11c6617c393607bb5427bcc2
Dustin Paltz <Dustin Paltz>
2016-03-05 21:03:44 -0800

    Initial implementation of the EMS based on the wireframe

--- client/ems/ems.js
+++ client/ems/ems.js
@@ -3,5 +3,9 @@ Template.ems.helpers({
 })
 
 Template.ems.events({
-    
+    'click li': function(event){
+        event.preventDefault();
+        $("div#displayTable").hide();
+        $("div.table"+event.currentTarget.id).show();
+    }
 })
