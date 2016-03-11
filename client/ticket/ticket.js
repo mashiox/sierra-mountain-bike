f5e419bd57200a78b5b44cc5cee01c4315b7811a
bflo70 <beto.flores70@gmail.com>
2016-03-10 20:58:09 -0800

    SMB-77 This is the final commit for the insert new ticket button.

--- client/ticket/ticket.js
+++ client/ticket/ticket.js
@@ -40,60 +40,39 @@ Template.tickets.events({
     
     'click button#newticket': function(event){
         event.preventDefault();
-        // Find a <table> element with id="myTable":
-        var table = document.getElementById("ticketTable");
-        // Create an empty <tr> element and add it to the 1st position of the table:
-        var row = table.insertRow();
         // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
-        var "name" = events.target.name.value;
-        var cell2 = row.insertCell(1);
-        var cell3 = row.insertCell(2);
-        var cell4 = row.insertCell(3);
-        var cell5 = row.insertCell(4);
-        var cell6 = row.insertCell(5);
-        var cell7 = row.insertCell(6);
-        var cell8 = row.insertCell(7);
-        var cell9 = row.insertCell(8);
+        var name = $("input#name").val();
+        var address = $("input#address").val();
+        var phone = $("input#phone").val();
+        
+        var problem = $("input#problem").val();
+        var cost = $("input#cost").val();
+        var status = $("input#status").val();
+        var resolution = $("input#resolution").val();
+        var description = $("input#description").val();
+        
         //Insert cell data into database
-        // Tickets.insert({
-        //    Name: name,
-        //    Address: "Enter Address", 
-        //    Phone: "Enter Phone Number",
-        //    Date: "Enter Date Brought In",
-        //    Problem: "Enter Problem",
-        //    Cost: "Enter Estimated Cost",
-        //    Status: "Enter Status",
-        //    Resolution: "Enter Resolution",
-        //    Description: "Enter Description",
-        // });
+         Tickets.insert({
+            Name: name,
+            Address: address, 
+            Phone: phone,
+            Date: new Date().toString(),
+            Problem: problem,
+            Cost: cost,
+            Status: status,
+            Resolution: resolution,
+            Description: description,
+         });
+         //Clear out data in form
+        $("input#name").val("");
+        $("input#address").val("");
+        $("input#phone").val("");
         
-        // Add some text to the new cells:
-        
-        cell1.innerText = "Enter Name";
-        cell2.innerHTML = "Enter Address";
-        cell3.innerHTML = "Enter Phone Number";
-        cell4.innerHTML = "Enter Date Brought In";
-        cell5.innerHTML = "Enter Problem";
-        cell6.innerHTML = "Enter Estimated Cost";
-        cell7.innerHTML = "Enter Status";
-        cell8.innerHTML = "Enter Resolution";
-        cell9.innerHTML = "Enter Description";
-        // Make content editable
-        row.contentEditable = true;
+        $("input#problem").val("");
+        $("input#cost").val("");
+        $("input#status").val("");
+        $("input#resolution").val("");
+        $("input#description").val("");
     }
 })
 
-
-Template.taskNotes.helpers({
-    optionsHelper: function(){
-        return {
-                textarea: true,
-                collection: "smbc_tickets",
-                field: "Name",
-                acceptEmpty: true,
-                placeholder: "Enter notes",
-                substitute: "Enter notes",
-                removeEmpty: false
-        };
-    }
-})
