dcba0a22009f2c5e8631028b2b4e04a2846df7a6
bflo70 <beto.flores70@gmail.com>
2016-04-06 20:22:51 -0700

    SMB-78 This is the commit for the complete export button

--- client/ticket/ticket.js
+++ client/ticket/ticket.js
@@ -157,12 +157,28 @@ Template.tickets.events({
 		document.getElementById("selectEditTicketStatus").disabled = true;
 	},
 
-	'click button#export': function (event) {
-		event.preventDefault();
-        var data = Tickets.find({}).fetch();
-        var csv = Papa.unparse(data);     
-        window.open('data:application/csv;charset=utf-8,' + csv);
-	}
+    'click button#export': function(event) {
+        event.preventDefault();
+        var csvArray = []
+        var allTicket = Tickets.find({}).fetch();
+        allTicket.forEach(function(row) {
+            var customer = Customers.findOne({ _id: row.CustomerId });
+            var problem = CommonProblems.findOne({ _id: row.Problem });
+
+            csvObj = {
+                "Customer_Name": customer.Name,
+                "Date": row.Date,
+                "Customer_Phone": customer.Phone,
+                "Customer_Address": customer.Address,
+                "Problem": problem.Description,
+                "Problem_Cost": problem.Cost,
+                "Status": row.Status,
+                "Notes": row.Notes
+            }
+            csvArray.push(csvObj);
+        });
+       JSONToCSVConvertor(csvArray, "Ticket Data", true);
+    }
 })
 
 Template.dialogEditTicket.helpers({
@@ -407,4 +423,79 @@ var renderTmp = function (template, data) {
 	document.body.appendChild(node);
 	UI.renderWithData(template, data, node);
 	return node;
+}
+
+
+//USED FOR THE EXPORT BUTTON
+function JSONToCSVConvertor(JSONData, ReportTitle, ShowLabel) {
+    //If JSONData is not an object then JSON.parse will parse the JSON string in an Object
+    var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
+    
+    var CSV = '';    
+    //Set Report title in first row or line
+    
+    CSV += ReportTitle + '\r\n\n';
+
+    //This condition will generate the Label/Header
+    if (ShowLabel) {
+        var row = "";
+        
+        //This loop will extract the label from 1st index of on array
+        for (var index in arrData[0]) {
+            
+            //Now convert each value to string and comma-seprated
+            row += index + ',';
+        }
+
+        row = row.slice(0, -1);
+        
+        //append Label row with line break
+        CSV += row + '\r\n';
+    }
+    
+    //1st loop is to extract each row
+    for (var i = 0; i < arrData.length; i++) {
+        var row = "";
+        
+        //2nd loop will extract each column and convert it in string comma-seprated
+        for (var index in arrData[i]) {
+            row += '"' + arrData[i][index] + '",';
+        }
+
+        row.slice(0, row.length - 1);
+        
+        //add a line break after each row
+        CSV += row + '\r\n';
+    }
+
+    if (CSV == '') {        
+        alert("Invalid data");
+        return;
+    }   
+    
+    //Generate a file name
+    var fileName = "MyReport_";
+    //this will remove the blank-spaces from the title and replace it with an underscore
+    fileName += ReportTitle.replace(/ /g,"_");   
+    
+    //Initialize file format you want csv or xls
+    var uri = 'data:text/csv;charset=utf-8,' + escape(CSV);
+    
+    // Now the little tricky part.
+    // you can use either>> window.open(uri);
+    // but this will not work in some browsers
+    // or you will not get the correct file extension    
+    
+    //this trick will generate a temp <a /> tag
+    var link = document.createElement("a");    
+    link.href = uri;
+    
+    //set the visibility hidden so it will not effect on your web-layout
+    link.style = "visibility:hidden";
+    link.download = fileName + ".csv";
+    
+    //this part will append the anchor tag and remove it after automatic click
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
 }
