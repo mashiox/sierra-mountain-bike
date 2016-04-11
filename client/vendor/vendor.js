f64258af5ab50eb88b83403159efd39af8889a72
Dustin Paltz <Dustin Paltz>
2016-04-10 17:24:19 -0700

    Initial commit to add functionality for managing vendor orders.
    Added packages:
    meteor add rajit:bootstrap3-datepicker

--- client/vendor/vendor.js
+++ client/vendor/vendor.js
@@ -2,22 +2,33 @@ Template.vendor.helpers({
 })
 
 Template.vendor.events({
+	'click li': function (event) {
+		paginationClickHelper(event);
+	},
+
+	'submit .new-search': function (event) {
+		events.preventDefault();
+		event.target.text.value = "";
+	}
 })
 
-Template.vendorTable.helpers({
+
+
+
+Template.vendors.helpers({
 	AllVendorsViewSettings: function () {
 		return {
 			collection: Vendors,
 			rowsPerPage: 5,
 			showFilter: true,
-			fields: ["Name", "Address", "Status",
+			fields: ["Name", "Status",
 				{ key: 'options', label: 'Options', tmpl: Template.vendorOptionsColumn }
 			]
 		}
 	}
 })
 
-Template.vendorTable.events({
+Template.vendors.events({
 	'click button#btnAddVendor': function (event) {
 		event.preventDefault();
 
@@ -121,11 +132,412 @@ Template.vendorTable.events({
 			showCancelButton: true,
 			closeOnConfirm: true,
 		},
-			function () {
-				Vendors.remove(obj._id);
-			});
+		function () {
+			Vendors.remove(obj._id);
+		});
+	},
+
+
+})
+
+
+
+
+
+
+Template.dialogEditVendorOrder.rendered = function () {
+	$('#txtEditVendorOrderReceiptDate').datepicker({
+		todayBtn: "linked",
+		orientation: "bottom auto",
+		autoclose: true,
+		todayHighlight: true
+	});
+}
+
+
+
+
+Template.dialogEditVendorOrder.helpers({
+	All_Vendors: function () {
+		return Vendors.find();
 	}
 })
+
+
+
+
+
+
+
+
+
+Template.orders.helpers({
+	AllOrdersViewSettings: function () {
+		return {
+			collection: VendorOrders,
+			rowsPerPage: 5,
+			showFilter: true,
+			fields: [
+				{
+					key: 'VendorID',
+					label: 'Vendor',
+					fn: function (value, object, key) {
+						return Vendors.findOne({ _id: value }).Name;
+					}
+				},
+				{ key: 'Status', label: 'Status' },
+				{
+					key: '_id',
+					label: 'Total',
+					fn: function (value, object, key) {
+						return OrderCost(value);
+					}
+				},
+				{ key: 'options', label: 'Options', tmpl: Template.vendorOrderOptionsColumn }
+			]
+		}
+	}
+})
+
+
+
+
+
+
+
+Template.orders.events({
+	'click button#btnAddVendorOrder': function (event) {
+		event.preventDefault();
+
+		var obj = this;
+
+		bootbox.dialog({
+			title: "Create new vendor order",
+			onEscape: true,
+			backdrop: true,
+			message: renderTmp(Template.dialogEditVendorOrder),
+			buttons: {
+				success: {
+					label: "Create",
+					className: "btn-success",
+					callback: function () {
+						var vendorID = $('#selectEditVendorOrderVendorID').val();
+						var deliveryAddress = $('#txtEditVendorOrderDeliveryAddress').val().trim();
+						var shippingType = $('#selectEditVendorOrderShippingType').val();
+						var receiptDateStr = $('#txtEditVendorOrderReceiptDate').val();
+						var status = $('#selectEditVendorOrderStatus').val();
+
+						var vendor = Vendors.findOne({ _id: vendorID });
+						if (vendor == null) {
+							swal('Oops...', 'Select a vendor!', 'error');
+							return false;
+						}
+
+						if (deliveryAddress == '') {
+							swal('Oops...', 'Enter a delivery address!', 'error');
+							return false;
+						}
+
+						var receiptDate = new Date(receiptDateStr);
+						if (isNaN(receiptDate)) {
+							swal('Oops...', 'Enter a receipt date!', 'error');
+							return false;
+						}
+
+						VendorOrders.insert({
+							VendorID: vendorID,
+							DeliveryAddress: deliveryAddress,
+							ShippingType: shippingType,
+							ReceiptDate: receiptDate,
+							Status: status
+						});
+
+						swal('Success!', 'Vendor added!', 'success');
+						return true;
+					}
+				}
+			}
+		});
+	},
+
+
+
+
+	
+
+
+	'click button#btnEditVendorOrderSettings': function (event) {
+		event.preventDefault();
+
+		var obj = this;
+		activeVendorOrderID = obj._id;
+
+		bootbox.dialog({
+			title: "Edit vendor order",
+			onEscape: true,
+			backdrop: true,
+			message: renderTmp(Template.dialogEditVendorOrder),
+			buttons: {
+				success: {
+					label: "Update",
+					className: "btn-success",
+					callback: function () {
+						var vendorID = $('#selectEditVendorOrderVendorID').val();
+						var deliveryAddress = $('#txtEditVendorOrderDeliveryAddress').val().trim();
+						var shippingType = $('#selectEditVendorOrderShippingType').val();
+						var receiptDateStr = $('#txtEditVendorOrderReceiptDate').val();
+						var status = $('#selectEditVendorOrderStatus').val();
+
+						var vendor = Vendors.findOne({ _id: vendorID });
+						if (vendor == null) {
+							swal('Oops...', 'Select a vendor!', 'error');
+							return false;
+						}
+
+						if (deliveryAddress == '') {
+							swal('Oops...', 'Enter a delivery address!', 'error');
+							return false;
+						}
+
+						var receiptDate = new Date(receiptDateStr);
+						if (isNaN(receiptDate)) {
+							swal('Oops...', 'Enter a receipt date!', 'error');
+							return false;
+						}
+
+						VendorOrders.update(
+							{ _id: obj._id },
+							{
+								$set: {
+									DeliveryAddress: deliveryAddress,
+									ShippingType: shippingType,
+									ReceiptDate: receiptDate,
+									Status: status
+								}
+							});
+
+						swal('Success!', 'Vendor added!', 'success');
+						return true;
+					}
+				}
+			}
+		});
+
+		document.getElementById('selectEditVendorOrderVendorID').value = this.VendorID;
+		document.getElementById('selectEditVendorOrderVendorID').disabled = true;
+		document.getElementById('txtEditVendorOrderDeliveryAddress').value = this.DeliveryAddress;
+		document.getElementById('selectEditVendorOrderShippingType').value = this.ShippingType;
+		document.getElementById('txtEditVendorOrderReceiptDate').value = this.ReceiptDate;
+		document.getElementById('selectEditVendorOrderStatus').value = this.Status;
+
+		if (this.Status == "Shipped") {
+			document.getElementById('txtEditVendorOrderDeliveryAddress').disabled = true;
+			document.getElementById('selectEditVendorOrderShippingType').disabled = true;
+			document.getElementById('txtEditVendorOrderReceiptDate').disabled = true;
+			document.getElementById('selectEditVendorOrderStatus').disabled = true;
+		}
+	},
+
+
+
+
+
+
+	'click button#btnEditVendorOrderItems': function (event) {
+		event.preventDefault();
+
+		var obj = this;
+
+		if (obj.Status == "Shipped") {
+			swal('Oops...', 'This order has shipped!', 'error');
+			return false;
+		}
+
+		activeVendorOrderID = obj._id;
+
+		bootbox.dialog({
+			title: "Edit vendor order items",
+			onEscape: true,
+			backdrop: true,
+			size: 'large',
+			message: renderTmp(Template.dialogEditVendorOrderItems),
+			buttons: {
+				success: {
+					label: "Done",
+					className: "btn-success",
+					callback: function () {
+
+						return true;
+					}
+				}
+			}
+		});
+	},
+})
+
+
+
+Template.dialogEditVendorOrderItems.helpers({
+	All_Inventory: function () {
+		return Inventory.find();
+	},
+
+	DynamicVendorOrderItems: function () {
+		return {
+			collection: VendorOrderItems.find({ OrderID: activeVendorOrderID }),
+			rowsPerPage: 5,
+			showFilter: true,
+			fields: [
+				{
+					key: 'ProductID',
+					label: 'Name',
+					fn: function (value, object, key) {
+						var product = Inventory.findOne({ _id: value });
+						return product.name;
+					}
+				},
+				{
+					key: 'ProductID',
+					label: 'Unit Cost',
+					fn: function (value, object, key) {
+						return ProductCost(value);
+					}
+				},
+				{ key: 'Quantity', label: 'Quantity' },
+				{ key: 'options', label: 'Options', tmpl: Template.vendorOrderItemsOptionsColumn }
+			]
+		}
+	}
+})
+
+
+
+
+
+Template.dialogEditVendorOrderItems.events({
+	'click button#btnEditVendorOrderItemsAddItem': function (event) {
+		event.preventDefault();
+		
+		var productID = $('#selectEditVendorOrderItemInventory').val();
+		var itemQuantity = $('#txtEditVendorOrderItemsQuantity').val();
+
+		if (productID == '') {
+			swal('Oops...', 'Select an inventory product!', 'error');
+			return false;
+		}
+
+		if (itemQuantity == '' || isNaN(itemQuantity)) {
+			swal('Oops...', 'Enter a valid quantity!', 'error');
+			return false;
+		}
+
+		VendorOrderItems.insert({
+			OrderID: activeVendorOrderID,
+			ProductID: productID,
+			Quantity: itemQuantity
+		});
+
+		$('#txtEditVendorOrderItemsQuantity').val("");
+		document.getElementById('selectEditVendorOrderItemInventory').selectedIndex = 0;
+
+		swal({
+			title: "Success",
+			text: "Item added to order.",
+			timer: 1200,
+			showConfirmButton: false
+		});
+	},
+
+
+
+
+	'click button#btnEditVendorOrderItemQuantity': function (event) {
+		event.preventDefault();
+
+		var obj = this;
+
+		bootbox.prompt("Enter new quantity", function (result) {
+			if (result === null || result == "") {
+				return;
+			}
+
+			newQuantity = result.trim();
+
+			if (!isNaN(newQuantity)) {
+				VendorOrderItems.update({ _id: obj._id }, { $set: { Quantity: newQuantity } });
+			}
+		});
+	},
+
+
+
+
+
+	'click button#btnEditVendorOrderItemRemove': function (event) {
+		event.preventDefault();
+
+		var obj = this;
+
+		swal({
+			title: "Confirm",
+			text: "Delete order item",
+			type: "info",
+			showCancelButton: true,
+			closeOnConfirm: true,
+		},
+		function () {
+			VendorOrderItems.remove(obj._id);
+		});
+	}
+})
+
+
+
+
+function OrderCost(orderID) {
+	var total = 0;
+
+	VendorOrderItems.find({ OrderID: orderID }).map(function (doc) {
+		var product = Inventory.findOne({ _id: doc.ProductID });
+		total += (product.cost * doc.Quantity);
+	});
+
+	return "$" + FormatMoney(total, 2);
+}
+
+function ProductCost(productID)
+{
+	var product = Inventory.findOne({ _id: productID });
+	return "$" + FormatMoney(product.cost, 2);
+}
+
+function FormatMoney(n, c) {
+	var c = isNaN(c = Math.abs(c)) ? 2 : c,
+		d = d == undefined ? "." : d,
+		t = t == undefined ? "," : t,
+		s = n < 0 ? "-" : "",
+		i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "",
+		j = (j = i.length) > 3 ? j % 3 : 0;
+	return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+}
+
+
+
+
+
+
+
+
+
+
+
+var activeVendorOrderID = null;
+
+
+
+
+
+
 
 var renderTmp = function (template, data) {
 	var node = document.createElement("div");
